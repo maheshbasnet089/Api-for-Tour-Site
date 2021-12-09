bfc27da3933fe3233fb9c6009ef6f7383298f54a
manishbasnet089 <basnetmanish089@gmail.com>
2021-12-09 19:04:22 +0545

    mongo Connection

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require("express");
-
 const app = express();
+const morgan = require("morgan");
+const mongoConnection = require("./controllers /db");
+if (process.env.NODE_ENV == "developement") {
+  app.use(morgan("tiny"));
+}
 
+mongoConnection(process.env.MONGO_URI);
 module.exports = app;
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const app = require("./app");
+const dotenv = require("dotenv");
+dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log(`Serving at port ${PORT}`);
+  console.log(`serving at port ${PORT}`);
 });
