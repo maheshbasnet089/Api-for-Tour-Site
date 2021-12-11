92184a882fe3170ae08d8d28eada6b074d567fc7
manishbasnet089 <basnetmanish089@gmail.com>
2021-12-11 12:52:05 +0545

    Update Route

--- controllers /tourController.js
+++ controllers /tourController.js
@@ -66,6 +66,25 @@ function tourController() {
         });
       }
     },
+    async updateTour(req, res) {
+      try {
+        const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+          new: true,
+          runValidators: true,
+        });
+        res.status(200).json({
+          status: "success",
+          data: {
+            tour,
+          },
+        });
+      } catch (e) {
+        res.status(400).json({
+          status: "fail",
+          message: e,
+        });
+      }
+    },
   };
 }
 
--- routes /tourRoute.js
+++ routes /tourRoute.js
@@ -5,5 +5,6 @@ router.get("/tours", tourController().getAllTours);
 router.get("/tours/:id", tourController().getTour);
 router.post("/tours", tourController().createTour);
 router.get("/tours/delete/:id", tourController().deleteTour);
+router.patch("/tours/update/:id", tourController().updateTour);
 
 module.exports = router;
