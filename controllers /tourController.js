0c8f41d6ad0753394bb1408e459ae34fa7f2c01c
manishbasnet089 <basnetmanish089@gmail.com>
2021-12-12 19:36:43 +0545

    aggreattion pipeline for Tour Stats

--- controllers /tourController.js
+++ controllers /tourController.js
@@ -96,6 +96,44 @@ function tourController() {
         });
       }
     },
+    async getTourStats(req, res) {
+      try {
+        const stats = await Tour.aggregate([
+          {
+            $match: { ratingsAverage: { $gte: 4.5 } },
+          },
+          {
+            $group: {
+              _id: { $toUpper: "$difficulty" },
+              numTours: { $sum: 1 },
+              numRatings: { $sum: "$ratingsQuantity" },
+              avgRating: { $avg: "$ratingsAverage" },
+              avgPrice: { $avg: "$price" },
+              minPrice: { $min: "$price" },
+              maxPrice: { $max: "$price" },
+            },
+          },
+          {
+            $sort: { avgPrice: 1 },
+          },
+          // {
+          //   $match: { _id: { $ne: 'EASY' } }
+          // }
+        ]);
+
+        res.status(200).json({
+          status: "success",
+          data: {
+            stats,
+          },
+        });
+      } catch (err) {
+        res.status(404).json({
+          status: "fail",
+          message: err,
+        });
+      }
+    },
   };
 }
 
--- routes /tourRoute.js
+++ routes /tourRoute.js
@@ -2,10 +2,13 @@ const tourController = require("../controllers /tourController");
 const router = require("express").Router();
 
 router.get("/tours", tourController().getAllTours);
+router.get("/tours/tour-stats", tourController().getTourStats);
+
 router.get("/tours/:id", tourController().getTour);
+
 router.post("/tours", tourController().createTour);
 router.get("/tours/delete/:id", tourController().deleteTour);
 router.patch("/tours/update/:id", tourController().updateTour);
-router.get("/tours/topTours", tourController().topTours);
+// router.get("/tours/topTours", tourController().topTours);
 
 module.exports = router;
