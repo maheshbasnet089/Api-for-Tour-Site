1316504147fa088af0a6487eec29aea02ba5487e
manishbasnet089 <basnetmanish089@gmail.com>
2021-12-20 09:05:08 +0545

    error : global middleware

--- app.js
+++ app.js
@@ -9,5 +9,24 @@ if (process.env.NODE_ENV == "developement") {
 
 app.use(express.json());
 
-app.use("/api/v1", router);
+app.use("/api/v1/tours", router);
+
+app.all("*", (req, res, next) => {
+  const err = new Error(`Cannot find path ${req.originalUrl} `);
+  err.status = "fail";
+  err.statusCode = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.status = err.status || "error";
+  err.statusCode = err.statusCode || 500;
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 module.exports = app;
+
+//git rm -r --cached node_modules
--- routes /tourRoute.js
+++ routes /tourRoute.js
@@ -1,15 +1,15 @@
 const tourController = require("../controllers /tourController");
 const router = require("express").Router();
 
-router.get("/tours", tourController().getAllTours);
-router.get("/tours/tour-stats", tourController().getTourStats);
-router.get("/tours/getMonthlyPlan/:year", tourController().getTourStats);
+router.get("/", tourController().getAllTours);
+router.get("/tour-stats", tourController().getTourStats);
+router.get("/getMonthlyPlan/:year", tourController().getTourStats);
 
 router.get("/tours/:id", tourController().getTour);
 
-router.post("/tours", tourController().createTour);
-router.get("/tours/delete/:id", tourController().deleteTour);
-router.patch("/tours/update/:id", tourController().updateTour);
+router.post("/", tourController().createTour);
+router.get("/delete/:id", tourController().deleteTour);
+router.patch("/update/:id", tourController().updateTour);
 // router.get("/tours/topTours", tourController().topTours);
 
 module.exports = router;
