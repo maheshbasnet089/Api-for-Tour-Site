16436f396260c8b70c7bcec15a911e593bae4562
manishbasnet089 <basnetmanish089@gmail.com>
2021-12-23 15:33:12 +0545

    error : Cast Error

--- controllers /errorController.js
+++ controllers /errorController.js
@@ -1,10 +1,45 @@
+const AppError = require("../utils /appError");
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path} : ${err.value}`;
+  return new AppError(message, 400);
+};
+
+function sendErrorDev(err, res) {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    error: err,
+    stack: err.stack,
+  });
+}
+
+function sendErrorProd(err, res) {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error("ERROR ✨", err);
+    res.status(500).json({
+      status: "error",
+      message: "something went very wrong",
+    });
+  }
+}
+
 module.exports = (err, req, res, next) => {
   //   console.log(err.stack);
   err.status = err.status || "error";
   err.statusCode = err.statusCode || 500;
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    // console.log("err", err);
+
+    if (err.name === "CastError") err = handleCastErrorDB(err);
+    // erroisOperational = true;
+    sendErrorProd(err, res);
+  }
 };
