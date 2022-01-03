f00e5bcb5c8239dffa53e55d23305cd9e8d95c15
manishbasnet089 <basnetmanish089@gmail.com>
2022-01-03 15:39:31 +0545

    req.body modified

--- controllers /authController.js
+++ controllers /authController.js
@@ -1,7 +1,12 @@
 const User = require("../model/userModel");
 
 exports.signUp = async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    password: req.body.password,
+    password: req.body.passwordConfirm,
+    email: req.body.email,
+  });
   res.status(201).json({
     status: "success",
     data: {
