1ccddfde64db7d10a3ce2364419c4facc08faadb
manishbasnet089 <basnetmanish089@gmail.com>
2022-01-03 15:52:12 +0545

    jwt token sign in

--- controllers /authController.js
+++ controllers /authController.js
@@ -1,14 +1,21 @@
 const User = require("../model/userModel");
+const jwt = require("jsonwebtoken");
 
 exports.signUp = async (req, res, next) => {
   const newUser = await User.create({
     name: req.body.name,
     password: req.body.password,
-    password: req.body.passwordConfirm,
+    passwordConfirm: req.body.passwordConfirm,
     email: req.body.email,
+  });
+
+  //jsonwebtoken signin
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
   });
   res.status(201).json({
     status: "success",
+    token,
     data: {
       user: newUser,
     },
